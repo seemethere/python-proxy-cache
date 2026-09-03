e379845b4e2d48727a91feb2d821758b53a0cfdb
Eli Uriegas <1700823+seemethere@users.noreply.github.com>
2026-09-03 12:24:39 -0700

    Microcache metadata-pending project responses (#16)

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,23 @@
 from httpx import Response
+from starlette.responses import Response as StarletteResponse
+
+from app.main import _vary_on_accept
 
 HTML_UPSTREAM = """<!DOCTYPE html><html><body>
 <a href="https://files.pythonhosted.org/packages/a/requests-2.31.0-py3-none-any.whl#sha256=abc">requests-2.31.0-py3-none-any.whl</a>
 </body></html>"""
+
+
+def test_vary_accept_preserves_existing_values_without_duplicates():
+    response = StarletteResponse(headers={"Vary": "Origin, accept"})
+    _vary_on_accept(response)
+    assert response.headers["vary"] == "Origin, accept"
+
+
+def test_vary_wildcard_is_not_modified():
+    response = StarletteResponse(headers={"Vary": "*"})
+    _vary_on_accept(response)
+    assert response.headers["vary"] == "*"
 
 
 async def test_synthesis_html_upstream_serves_json(client, mock_upstream):
